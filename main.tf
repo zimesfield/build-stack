fd9125702eb3986acef8d4db958517f3bd424786
cyprian.omenuko <cyprian.omenuko@unite.eu>
2024-09-24 19:57:24 +0100

    Make dir to keep environment variables

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "keycloak-server" {
   app_name           = "keycloak-server"
   username           = var.keycloak_admin_username
   password           = var.keycloak_admin_password
-  db_host            = module.keycloak-database.database_host
+  db_host            = "keycloak-database-postgresql"
   db_port            = var.keycloak_port
   db_name            = var.keycloak_database
   db_username        = var.keycloak_database_username
