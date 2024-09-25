b02a20843856979c197cb7759889f9ad1d06ca42
cyprian.omenuko <cyprian.omenuko@unite.eu>
2024-09-25 14:54:14 +0100

    Fixed formating

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
 module "backend" {
-  source             = "git::git@github.com:zimesfield/infrastructure-stack.git//modules/storage?ref=v.0.1.8"
-  object_storage_key = "terraform_state"
-  region             = "eu-central-1"
-  token              = var.token
-  file_path          = var.terraform_env_state_file
+  source               = "git::git@github.com:zimesfield/infrastructure-stack.git//modules/storage?ref=v.0.1.8"
+  object_storage_key   = "terraform_state"
+  region               = "eu-central-1"
+  token                = var.token
+  file_path            = var.terraform_env_state_file
   storage_bucket_label = "terraform.tfstate"
-  storage_key_label = "terraform.tfstate"
+  storage_key_label    = "terraform.tfstate"
 }
 
 
--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     linode = {
-      source = "linode/linode"
+      source  = "linode/linode"
       version = "2.28.0"
     }
     helm = {
